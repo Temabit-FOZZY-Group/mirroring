89366f251675fc3ff0dcca9f11d0f5ae15dc40ca
Sukhryn Alexandr <alexandrvirtual@gmail.com>
2023-01-09 20:19:58 +0200

    fix tests

--- src/test/scala/mirroring/FilterBuilderSuite.scala
+++ src/test/scala/mirroring/FilterBuilderSuite.scala
@@ -21,18 +21,7 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions.{col, lit}
 import org.scalatest.funsuite.AnyFunSuite
 
-class FilterBuilderSuite extends AnyFunSuite with SparkSessionLender {
-
-  def withLocalSparkContext(testFunction: SparkSession => Any): Unit = {
-    val spark = SparkSession
-      .builder()
-      .appName("spark testing")
-      .master("local")
-      .getOrCreate()
-    try {
-      testFunction(spark)
-    } finally spark.stop()
-  }
+class FilterBuilderSuite extends AnyFunSuite {
 
   test("buildJoinCondition should return String with join clause") {
     val primaryKey: Array[String] = Array("id", "FilId", "id2")
@@ -48,185 +37,187 @@ class FilterBuilderSuite extends AnyFunSuite with SparkSessionLender {
     assert(result.equals("test_some_chars__"))
   }
 
-  test(
-    "buildStrWithoutSpecChars should replace special chars in input with __"
-  ) {
+  test("buildStrWithoutSpecChars should replace special chars in input with __") {
     val input  = "test_some*chars&."
     val result = FilterBuilder.buildStrWithoutSpecChars(input, "__")
-    println(result)
     assert(result.equals("test_some__chars____"))
   }
 
-//  test(
-//    "buildReplaceWherePredicate should collect values from dataset to replace them on target"
-//  ) {
-//    withLocalSparkContext(spark => {
-//      val partitionCol = "id"
-//      import spark.implicits._
-//      val df = spark.sparkContext
-//        .parallelize(
-//          Seq((1, "2019-10-05", "00", "A"), (2, "2019-10-05", "01", "B"))
-//        )
-//        .toDF("id", "date", "hour", "content")
-//      val result = FilterBuilder.buildReplaceWherePredicate(df, partitionCol, "")
-//      assert(result == "id in (1, 2)")
-//    })
-//  }
+  test(
+    "buildReplaceWherePredicate should collect values from dataset to replace them on target"
+  ) {
+    val spark = SparkSession
+      .builder()
+      .appName("spark testing")
+      .master("local")
+      .getOrCreate()
+    val partitionCol = "id"
+    import spark.implicits._
+    val df = spark.sparkContext
+      .parallelize(
+        Seq((1, "2019-10-05", "00", "A"), (2, "2019-10-05", "01", "B"))
+      )
+      .toDF("id", "date", "hour", "content")
+    val result = FilterBuilder.buildReplaceWherePredicate(df, partitionCol, "")
+    assert(result == "id in (1, 2)")
+  }
 
-//  test("buildReplaceWherePredicate with empty partition") {
-//    withLocalSparkContext(spark => {
-//      val partitionCol = ""
-//      import spark.implicits._
-//      val df = spark.sparkContext
-//        .parallelize(
-//          Seq((1, "2019-10-05", "00", "A"), (2, "2019-10-05", "01", "B"))
-//        )
-//        .toDF("id", "date", "hour", "content")
-//      val result = FilterBuilder.buildReplaceWherePredicate(df, partitionCol, "")
-//      assert(result == "")
-//    })
-//  }
+  test("buildReplaceWherePredicate with empty partition") {
+    val spark = SparkSession
+      .builder()
+      .appName("spark testing")
+      .master("local")
+      .getOrCreate()
+    val partitionCol = ""
+    import spark.implicits._
+    val df = spark.sparkContext
+      .parallelize(
+        Seq((1, "2019-10-05", "00", "A"), (2, "2019-10-05", "01", "B"))
+      )
+      .toDF("id", "date", "hour", "content")
+    val result = FilterBuilder.buildReplaceWherePredicate(df, partitionCol, "")
+    assert(result == "")
+  }
 
   test(
     "buildReplaceWherePredicate with empty partition and null dataframe"
   ) {
-    withLocalSparkContext(spark => {
-      val partitionCol = ""
-      val df           = null
-      val result       = FilterBuilder.buildReplaceWherePredicate(df, partitionCol, "")
-      assert(result == "")
-    })
+    val partitionCol = ""
+    val df           = null
+    val result       = FilterBuilder.buildReplaceWherePredicate(df, partitionCol, "")
+    assert(result == "")
   }
 
-//  test(
-//    "buildReplaceWherePredicate should collect values from dataset to replace them on target and append where clause from user"
-//  ) {
-//    withLocalSparkContext(spark => {
-//      val partitionCol = "id"
-//      val whereClause =
-//        "(filid = 3171 and cast(operationdate as date) = '2022-12-02')"
-//      import spark.implicits._
-//      val df = spark.sparkContext
-//        .parallelize(
-//          Seq((1, "2019-10-05", "00", "A"), (2, "2019-10-05", "01", "B"))
-//        )
-//        .toDF("id", "date", "hour", "content")
-//      val result = FilterBuilder.buildReplaceWherePredicate(df, partitionCol, whereClause)
-//      assert(
-//        result == "(filid = 3171 and cast(operationdate as date) = '2022-12-02') AND id in (1, 2)"
-//      )
-//    })
-//  }
+  test(
+    "buildReplaceWherePredicate should collect values from dataset to replace them on target and append where clause from user"
+  ) {
+    val partitionCol = "id"
+    val whereClause =
+      "(filid = 3171 and cast(operationdate as date) = '2022-12-02')"
+    val spark = SparkSession
+      .builder()
+      .appName("spark testing")
+      .master("local")
+      .getOrCreate()
+    import spark.implicits._
+    val df = spark.sparkContext
+      .parallelize(
+        Seq((1, "2019-10-05", "00", "A"), (2, "2019-10-05", "01", "B"))
+      )
+      .toDF("id", "date", "hour", "content")
+    val result = FilterBuilder.buildReplaceWherePredicate(df, partitionCol, whereClause)
+    assert(
+      result == "(filid = 3171 and cast(operationdate as date) = '2022-12-02') AND id in (1, 2)"
+    )
+  }
 
   test(
     "buildReplaceWherePredicate with empty partition and null dataframe and where clause from user should return where clause"
   ) {
-    withLocalSparkContext(spark => {
-      val partitionCol = ""
-      val df           = null
-      val whereClause =
-        "(filid = 3171 and cast(operationdate as date) = '2022-12-02')"
-      val result = FilterBuilder.buildReplaceWherePredicate(df, partitionCol, whereClause)
-      assert(result == "(filid = 3171 and cast(operationdate as date) = '2022-12-02')")
-    })
+    val partitionCol = ""
+    val df           = null
+    val whereClause = "(filid = 3171 and cast(operationdate as date) = '2022-12-02')"
+    val result = FilterBuilder.buildReplaceWherePredicate(df, partitionCol, whereClause)
+    assert(result == "(filid = 3171 and cast(operationdate as date) = '2022-12-02')")
   }
 
   test("buildMergeCondition should return String with condition") {
-    withLocalSparkContext(spark => {
-      val args: Array[String] = Array(
-        "path_to_save==dummy",
-        "jdbcUrl==dummy;user=hhh;password=ll",
-        "tab==dummy",
-        "change_tracking==True",
-        "primary_key==id,[FilId], id2"
+    val args: Array[String] = Array(
+      "path_to_save==dummy",
+      "jdbcUrl==dummy;user=hhh;password=ll",
+      "tab==dummy",
+      "change_tracking==True",
+      "primary_key==id,[FilId], id2"
+    )
+    val config = ConfigBuilder.build(ConfigBuilder.parse(args))
+    val result = FilterBuilder.buildMergeCondition(
+      config.primary_key,
+      sourceColPrefix = "SYS_CHANGE_PK_",
+      ds = null
+    )
+    assert(
+      result.equals(
+        "source.SYS_CHANGE_PK_id = target.id and source.SYS_CHANGE_PK_FilId = target.FilId " +
+          "and source.SYS_CHANGE_PK_id2 = target.id2"
       )
-      val config = ConfigBuilder.build(ConfigBuilder.parse(args))
-      val result = FilterBuilder.buildMergeCondition(
-        config.primary_key,
-        sourceColPrefix = "SYS_CHANGE_PK_",
-        ds = null
-      )
-      assert(
-        result.equals(
-          "source.SYS_CHANGE_PK_id = target.id and source.SYS_CHANGE_PK_FilId = target.FilId " +
-            "and source.SYS_CHANGE_PK_id2 = target.id2"
-        )
-      )
-    })
+    )
   }
 
   test("buildMergeCondition should return String with condition 2") {
-    withLocalSparkContext(spark => {
-      val args: Array[String] = Array(
-        "path_to_save==dummy",
-        "jdbcUrl==dummy;user=hhh;password=ll",
-        "tab==dummy",
-        "change_tracking==true",
-        "primary_key==id,[FilId], id2"
-      )
-      val config = ConfigBuilder.build(ConfigBuilder.parse(args))
-      val ds = spark
-        .range(3)
-        .withColumn("FilId", col("id").multiply(10))
-        .withColumn("id2", lit(0))
-      val result = FilterBuilder.buildMergeCondition(
-        config.primary_key,
-        partitionCol = "FilId",
-        ds = ds
-      )
-      val expectedResult =
-        "target.FilId in (0, 10, 20) and source.id = target.id and source.FilId = target.FilId " +
-          "and source.id2 = target.id2"
-      assert(result.equals(expectedResult))
-    })
+    val spark = SparkSession
+      .builder()
+      .appName("spark testing")
+      .master("local")
+      .getOrCreate()
+    val args: Array[String] = Array(
+      "path_to_save==dummy",
+      "jdbcUrl==dummy;user=hhh;password=ll",
+      "tab==dummy",
+      "change_tracking==true",
+      "primary_key==id,[FilId], id2"
+    )
+    val config = ConfigBuilder.build(ConfigBuilder.parse(args))
+    val ds = spark
+      .range(3)
+      .withColumn("FilId", col("id").multiply(10))
+      .withColumn("id2", lit(0))
+    val result = FilterBuilder.buildMergeCondition(
+      config.primary_key,
+      partitionCol = "FilId",
+      ds = ds
+    )
+    val expectedResult =
+      "target.FilId in (0, 10, 20) and source.id = target.id and source.FilId = target.FilId " +
+        "and source.id2 = target.id2"
+    assert(result.equals(expectedResult))
   }
 
   test("buildMergeCondition should return String with condition 3") {
-    withLocalSparkContext(spark => {
-      val args: Array[String] = Array(
-        "path_to_save==dummy",
-        "jdbcUrl==dummy;user=hhh;password=ll",
-        "tab==dummy",
-        "change_tracking==true",
-        "primary_key==id,[FilId], id2"
-      )
-      val config = ConfigBuilder.build(ConfigBuilder.parse(args))
-      val ds = spark
-        .range(3)
-        .withColumn("FilId", col("id").multiply(10))
-        .withColumn("id2", lit(0))
-      val result = FilterBuilder.buildMergeCondition(
-        config.primary_key,
-        partitionCol = "FilId",
-        ds = ds,
-        sourceColPrefix = "pref_",
-        targetColPrefix = "anotherPref_"
-      )
-      val expectedResult =
-        "target.FilId in (0, 10, 20) and source.pref_id = target.anotherPref_id and " +
-          "source.pref_FilId = target.anotherPref_FilId and source.pref_id2 = target.anotherPref_id2"
-      assert(result.equals(expectedResult))
-    })
+    val spark = SparkSession
+      .builder()
+      .appName("spark testing")
+      .master("local")
+      .getOrCreate()
+    val args: Array[String] = Array(
+      "path_to_save==dummy",
+      "jdbcUrl==dummy;user=hhh;password=ll",
+      "tab==dummy",
+      "change_tracking==true",
+      "primary_key==id,[FilId], id2"
+    )
+    val config = ConfigBuilder.build(ConfigBuilder.parse(args))
+    val ds = spark
+      .range(3)
+      .withColumn("FilId", col("id").multiply(10))
+      .withColumn("id2", lit(0))
+    val result = FilterBuilder.buildMergeCondition(
+      config.primary_key,
+      partitionCol = "FilId",
+      ds = ds,
+      sourceColPrefix = "pref_",
+      targetColPrefix = "anotherPref_"
+    )
+    val expectedResult =
+      "target.FilId in (0, 10, 20) and source.pref_id = target.anotherPref_id and " +
+        "source.pref_FilId = target.anotherPref_FilId and source.pref_id2 = target.anotherPref_id2"
+    assert(result.equals(expectedResult))
   }
 
   test("buildMergeCondition should return String with condition 4") {
-    withLocalSparkContext(spark => {
-      val args: Array[String] = Array(
-        "path_to_save==dummy",
-        "jdbcUrl==dummy;user=hhh;password=ll",
-        "tab==dummy",
-        "change_tracking==true",
-        "primary_key==id,[FilId], id2"
+    val args: Array[String] = Array(
+      "path_to_save==dummy",
+      "jdbcUrl==dummy;user=hhh;password=ll",
+      "tab==dummy",
+      "change_tracking==true",
+      "primary_key==id,[FilId], id2"
+    )
+    val config = ConfigBuilder.build(ConfigBuilder.parse(args))
+    val result = FilterBuilder.buildMergeCondition(config.primary_key, ds = null)
+    assert(
+      result.equals(
+        "source.id = target.id and source.FilId = target.FilId " +
+          "and source.id2 = target.id2"
       )
-      val config = ConfigBuilder.build(ConfigBuilder.parse(args))
-      val result =
-        FilterBuilder.buildMergeCondition(config.primary_key, ds = null)
-      assert(
-        result.equals(
-          "source.id = target.id and source.FilId = target.FilId " +
-            "and source.id2 = target.id2"
-        )
-      )
-    })
+    )
   }
 }
