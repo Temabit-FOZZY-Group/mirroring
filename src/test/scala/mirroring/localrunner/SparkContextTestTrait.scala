b0c1df9f1b4467762ab4c15f7bef3b4ba6eaad68
Orlov Vladislav <vl.orlov@temabit.com>
2023-06-29 09:29:16 +0300

    Remove commented

--- src/test/scala/mirroring/localrunner/SparkContextTestTrait.scala
+++ src/test/scala/mirroring/localrunner/SparkContextTestTrait.scala
@@ -28,25 +28,9 @@ trait SparkContextTestTrait extends SparkContextTrait {
         "spark.sql.catalog.spark_catalog",
         "org.apache.spark.sql.delta.catalog.DeltaCatalog"
       )
-//      .config("spark.delta.logStore.class", "org.apache.spark.sql.delta.storage.S3SingleDriverLogStore")
       .config("spark.sql.parquet.int96RebaseModeInWrite", "LEGACY")
       .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
       .config("spark.sql.catalogImplementation", "hive")
-//      .config("spark.sql.warehouse.dir", "s3a://minio-v2.palefat.fozzy.lan/palefat-internal-data/vl_orlov")
-//      .config(
-//        "spark.hadoop.hive.metastore.uris",
-//        "thrift://hive-metastore-0.hive-metastore-hs.hive-metastore.svc:9083,thrift://hive-metastore-1.hive-metastore-hs.hive-metastore.svc:9083"
-//      )
-//      .config("spark.hadoop.hive.metastore.uri.selection", "RANDOM")
-//      .config("spark.hadoop.hive.input.format", "io.delta.hive.HiveInputFormat")
-//      .config("spark.hadoop.hive.tez.input.format", "io.delta.hive.HiveInputFormat")
-//      .config("spark.hadoop.fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider")
-//      .config("spark.hadoop.fs.s3a.path.style.access", "true")
-//      .config("spark.hadoop.fs.s3a.access.key", "spark-jobs")
-//      .config("spark.hadoop.fs.s3a.secret.key", "1xnNUDM44rWk0V0PmuLYhgd8CCeC95FM")
-//      .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
-//      .config("spark.hadoop.fs.s3a.fast.upload", "true")
-//      .config("spark.hadoop.fs.s3a.endpoint", "minio-v2.palefat.fozzy.lan")
       .enableHiveSupport()
       .master("local[*]")
       .getOrCreate()
